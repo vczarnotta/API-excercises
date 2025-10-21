8ff89db62cc0e1d516c8db834b0936bc7b5ed282
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2025-10-21 15:45:40 +0200

    fox website

---
+++ fox-website/script.js
@@ -0,0 +1,26 @@
+const imageBtn = document.querySelector("button");
+const imageContainer = document.querySelector("div")
+
+const colors = ["#ee4266", "#ffd23f", "#3bceac", "#0ead69"]
+let colorIndex = 0
+
+imageBtn.addEventListener("click", () => {
+    document.body.classList.add("align-top")
+
+    //Hämta bild från API
+    fetch("https://randomfox.ca/floof/")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.image)
+        
+        //Hämta och visa bild
+        const foxImage = document.createElement("img");
+        foxImage.src = data.image
+        imageContainer.appendChild(foxImage)
+        foxImage.classList.add("fox-img")
+
+        //Sätt bakgrundsfärg
+        colorIndex = (colorIndex + 1) % colors.length
+        foxImage.style.backgroundColor = colors[colorIndex]
+    })
+});
